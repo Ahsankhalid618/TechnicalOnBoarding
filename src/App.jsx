64ca043a41a3e827a718a03c1728ebd146ffcc98
Ahsan khalid <83424436+Ahsankhalid618@users.noreply.github.com>
2023-09-02 00:03:26 +0500

    All changes saved

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Ahsan khalid",
+  name: "Ahsan khalid ",
   title: "Front-end developer",
   email: "ahsankhalid816@gmail.com",
   gitHub: "https://github.com/Ahsankhalid618",
